78c609c6de8a6ccf1ad968b9830cc67860fe07bc
Yang Wang <yangwang1@microsoft.com>
2024-08-12 08:11:35 +0000

    Merged PR 10633075: Make net_exporter_interval as argument
    Make net_exporter_interval as argument
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    New feature: Adds the ability to set `net_exporter_interval` as an argument.
    
    #### PR Summary
    This pull request introduces the capability to specify the network exporter update frequency via an argument.
    - `/src/job-exporter/build/moneo-net-exporter_entrypoint.sh`: Added `INTERVAL` argument to set the update frequency for the network exporter.
    - `/src/job-exporter/deploy/job-exporter.yaml.template`: Passed `net_exporter_interval` from the configuration to the entrypoint script.
    - `/src/job-exporter/config/job-exporter.yaml`: Added `net_exporter_interval` configuration with a default value of 5.

--- src/job-exporter/build/moneo-net-exporter_entrypoint.sh
+++ src/job-exporter/build/moneo-net-exporter_entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+INTERVAL="${1}"
+
 # Starts Network Exporter with specified InfiniBand sysfs path
 if lspci | grep -qi 'infiniband'; then
-    python3 /Moneo/src/worker/exporters/net_exporter.py --inifiband_sysfs=/hostsys/class/infiniband &
+    python3 /Moneo/src/worker/exporters/net_exporter.py --inifiband_sysfs=/hostsys/class/infiniband --update_freq=$INTERVAL &
     echo "Network Exporter Started!"
 else
     echo "/hostsys/class/infiniband not found, Network Exporter not started."
