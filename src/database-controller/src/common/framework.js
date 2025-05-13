224349e984c8a5babbb71084adf6508532e96414
Rui Gao <ruigao@microsoft.com>
2025-05-13 10:41:04 +0800

    fix the error code agfter the API updated

--- src/database-controller/src/common/framework.js
+++ src/database-controller/src/common/framework.js
@@ -438,7 +438,7 @@ class AddOns {
       try {
         await k8s.createSecret(this._configSecretDef);
       } catch (err) {
-        if (err.response && err.response.statusCode === 409) {
+        if (err.code === 409) {
           logger.warn(
             `Secret ${this._configSecretDef.metadata.name} already exists.`,
           );
@@ -451,7 +451,7 @@ class AddOns {
       try {
         await k8s.createSecret(this._userExtensionSecretDef);
       } catch (err) {
-        if (err.response && err.response.statusCode === 409) {
+        if (err.code === 409) {
           logger.warn(
             `Secret ${this._userExtensionSecretDef.metadata.name} already exists.`,
           );
@@ -464,7 +464,7 @@ class AddOns {
       try {
         await k8s.createPriorityClass(this._priorityClassDef);
       } catch (err) {
-        if (err.response && err.response.statusCode === 409) {
+        if (err.code === 409) {
           logger.warn(
             `PriorityClass ${this._priorityClassDef.metadata.name} already exists.`,
           );
@@ -477,7 +477,7 @@ class AddOns {
       try {
         await k8s.createSecret(this._dockerSecretDef);
       } catch (err) {
-        if (err.response && err.response.statusCode === 409) {
+        if (err.code === 409) {
           logger.warn(
             `Secret ${this._dockerSecretDef.metadata.name} already exists.`,
           );
@@ -490,7 +490,7 @@ class AddOns {
       try {
         await k8s.createSecret(this._tokenSecretDef);
       } catch (err) {
-        if (err.response && err.response.statusCode === 409) {
+        if (err.code === 409) {
           logger.warn(
             `Secret ${this._tokenSecretDef.metadata.name} already exists.`,
           );
@@ -574,7 +574,7 @@ async function synchronizeCreate(snapshot, addOns) {
     addOns.silentPatch(frameworkResponse);
     return frameworkResponse;
   } catch (err) {
-    if (err.response && err.response.statusCode === 409) {
+    if (err.code === 409) {
       // doesn't delete add-ons if 409 error
       logger.warn(`Framework ${snapshot.getName()} already exists.`);
       throw err;
@@ -606,7 +606,7 @@ async function synchronizeRequest(snapshot, addOns) {
     return frameworkResponse;
   } catch (err) {
     // If framework doesn't exist, create it.
-    if (err.response && err.response.statusCode === 404) {
+    if (err.code === 404) {
       const frameworkResponse = await synchronizeCreate(snapshot, addOns);
       logger.info(
         `Request of framework ${snapshot.getName()} is successfully created.`,
