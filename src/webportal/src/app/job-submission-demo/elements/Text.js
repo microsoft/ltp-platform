77ed2b32a3b3c8bf06a1f79c922048da6def12ba
Rui Gao <ruigao@microsoft.com>
2025-06-12 13:52:52 +0800

    remove webportal and submit-job-v2

--- src/webportal/src/app/job-submission-demo/elements/Text.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-import styled from 'styled-components';
-import { color, typography } from 'styled-system';
-
-const Text = styled('span')(color, typography);
-
-export default Text;
