6e7c052e05abd89ac7943bbeebebbecefb9699b2
Yifan Xiong <Yifan.Xiong@microsoft.com>
2025-06-13 02:01:03 +0000

    Merged PR 46: Fix sdk path and submodule checkout
    Fix sdk path and submodule checkout.

--- src/alert-manager/build/build-pre.sh
+++ src/alert-manager/build/build-pre.sh
@@ -5,7 +5,7 @@
 
 pushd $(dirname "$0") > /dev/null
 
-cp -arfT "../../kusto/sdk" "../src/node-recycler"
+cp -arfT "../../kusto-sdk" "../src/node-recycler/kusto-sdk"
 cp -arfT "../../database-controller/sdk" "../src/job-status-change-notification/openpaidbsdk"
 
 popd > /dev/null
