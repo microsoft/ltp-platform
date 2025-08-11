d70c921fc2d2954f5c649da5c7f2afe9b3dc6657
Yifan Xiong <Yifan.Xiong@microsoft.com>
2025-08-11 00:22:16 +0000

    Merged PR 107: Fix bugs in cluster local storage
    Fix bugs in cluster local storage service.

--- src/cluster-local-storage/src/service.py
+++ src/cluster-local-storage/src/service.py
@@ -23,9 +23,10 @@ from ltp_kusto_sdk import NodeStatusClient, NodeActionClient
 from ltp_kusto_sdk.features.node_status.models import NodeStatus
 
 logging.basicConfig(
-    level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(message)s",
 )
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
@@ -54,13 +55,14 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
         self._hostfile = "/tmp/hostfile"
         self._bin_dir = os.getenv("CLUSTER_LOCAL_STORAGE_BIN", "/usr/local/cluster-local-storage").rstrip("/")
         self._commands = {
-            "azcopy": "azcopy sync --output-level essential --log-level WARNING --recursive '{blob_dir}?{blob_token}' '{path}'",
-            "broadcast": f"{self._bin_dir}/bcast -h {self._hostfile} " + "-p {path}",
+            "azcopy": "\"azcopy sync --output-level essential --log-level WARNING --recursive '{blob_dir}?{blob_token}' '{path}'\"",
+            "broadcast": f"{self._bin_dir}/bcast.sh -h {self._hostfile} " + "-p {path}",
             "delete": "rm -rf {path}",
-            "size": f"du -sb {self.root}",
-            "sync": f"{self._bin_dir}/datasync {self.root} " + "{src} {dst}",
+            "size": f"\"find {self.root}" + " -type f -print0 | du -cb --files0-from=- | awk '/total/{print $1}'\"",
+            "sync": f"{self._bin_dir}/datasync.sh {self.root} " + "{src} {dst}",
         }
 
+        self._max_workers = int(os.getenv("CLUSTER_LOCAL_STORAGE_MAX_WORKERS", 64))
         self._shutdown_evt = threading.Event()
 
         super().__init__(bind, self._handler_factory())
@@ -70,7 +72,7 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
         )
         self._sync_thread.start()
 
-        logging.info(f"Serving {self.root} for {self.cluster} cluster on http://{bind[0]}:{bind[1]}")
+        logger.info(f"Serving {self.root} for {self.cluster} cluster on http://{bind[0]}:{bind[1]}")
 
     def _safe_path(self, rel):
         p = (self.root / rel).resolve()
@@ -81,23 +83,26 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
         return str(p)
 
     def _set_state(self, new):
-        logging.debug(f"State {self.state} -> {new}")
+        logger.debug(f"===== State {self.state} -> {new} =====")
         self.state = new
 
     def _get_node(self, data=True, num=None, write_file=False):
         status = NodeStatus.AVAILABLE if data else NodeStatus.AVAILABLE_NODATA
         hostnames = [n["HostName"] for n in self.status_client.get_nodes_by_status(status.value)] if self.status_client else []
         hostnames = list(filter(self.cluster_hostname_regex.search, hostnames))
+        logger.debug(f"Queried {len(hostnames)} {status.value} nodes: {','.join(hostnames)}")
         if write_file:
             with open(self._hostfile, "w") as f:
-                f.write("\n".join(hostnames))
+                f.write("\n".join(hostnames) + "\n")
+        if num and len(hostnames) < num:
+            raise RuntimeError(f"Cannot find {num} {status.value} nodes")
         return hostnames[:num] if num else hostnames
 
     def _update_node_status(self, hostname, data=True, reason=""):
         status = NodeStatus.AVAILABLE if data else NodeStatus.AVAILABLE_NODATA
         prev = NodeStatus.AVAILABLE_NODATA if data else NodeStatus.AVAILABLE
         # cordon or uncordon nodes
-        self.client.patch_node(
+        self.k8s.patch_node(
             name=hostname,
             body={"spec": {"unschedulable": (not data)}},
         )
@@ -115,38 +120,42 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
             )
 
     def _ensure_cordon(self):
-        logging.info("Cordoning nodes in AVAILABLE_NODATA status ...")
+        nodes = self._get_node(data=False)
+        if len(nodes) == 0:
+            logger.info("Skip cordon due to no available_nodata node")
+            return
+        logger.info(f"Cordoning {len(nodes)} nodes in available_nodata status ...")
         try:
-            for hostname in self._get_node(data=False):
-                self.client.patch_node(
+            for hostname in nodes:
+                self.k8s.patch_node(
                     name=hostname,
                     body={"spec": {"unschedulable": True}},
                 )
         except Exception as e:
-            logging.error(f"Cordon failed due to {e}")
+            logger.error(f"Cordon failed due to {e}")
             return
-        logging.info("Cordon done")
+        logger.info("Cordon done")
 
     def _download_from_blob(self, rel, blob_dir, blob_token):
         path = self._safe_path(rel)
         with self.lock:
             self._set_state("downloading")
-            logging.info(f"Downloading {path} from {blob_dir} ...")
+            logger.info(f"Downloading {path} from {blob_dir} ...")
             try:
                 node = self._get_node(data=True, num=1, write_file=True)[0]
                 # azcopy
                 cmd = f"ssh {node} {self._commands['azcopy'].format(path=path, blob_dir=blob_dir, blob_token=blob_token)}"
-                logging.debug(f"Execute command: {cmd}")
+                logger.debug(f"Execute command: {cmd}")
                 subprocess.run(cmd, shell=True, check=True)
-                logging.info(f"Azcopy {blob_dir} to {path} finished on {node}")
+                logger.info(f"Azcopy {blob_dir} to {path} finished on {node}")
                 # broadcast
                 cmd = f"scp {self._hostfile} {node}:{self._hostfile} && ssh {node} {self._commands['broadcast'].format(path=path)}"
-                logging.debug(f"Execute command: {cmd}")
+                logger.debug(f"Execute command: {cmd}")
                 subprocess.run(cmd, shell=True, check=True)
-                logging.info(f"Broadcast {path} finished on {node}")
+                logger.info(f"Broadcast {path} finished on {node}")
                 return True
             except Exception as e:
-                logging.error(f"Download failed due to: {e}")
+                logger.error(f"Download failed due to: {e}")
                 raise
             finally:
                 self._set_state("idle")
@@ -155,14 +164,14 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
         path = self._safe_path(rel)
         with self.lock:
             self._set_state("deleting")
-            logging.info(f"Deleting {path} ...")
+            logger.info(f"Deleting {path} ...")
             try:
                 nodes = self._get_node(data=True)
                 results = []
-                with ThreadPoolExecutor(max_workers=32) as executor:
+                with ThreadPoolExecutor(max_workers=self._max_workers) as executor:
                     for node in nodes:
                         cmd = f"ssh {node} {self._commands['delete'].format(path=path)}"
-                        logging.debug(f"Execute command: {cmd}")
+                        logger.debug(f"Execute command: {cmd}")
                         results.append(executor.submit(subprocess.run, cmd, shell=True))
                 failed = []
                 for res in results:
@@ -172,20 +181,20 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
                         failed.append(node)
                         self._update_node_status(node, data=False, reason="Deletion failed")
                 if failed:
-                    logging.warning(f"Delete failed on: {' '.join(failed)}")
-                logging.info(f"Delete {path} finished")
+                    logger.warning(f"Delete failed on: {' '.join(failed)}")
+                logger.info(f"Delete {path} finished")
                 return len(failed) == 0
             except Exception as e:
-                logging.error(f"Delete failed due to: {e}")
+                logger.error(f"Delete failed due to: {e}")
                 raise
             finally:
                 self._set_state("idle")
 
     def _size(self):
         with self.lock:
-            logging.info("Checking size ...")
+            logger.info("Checking size ...")
             cmd = f"ssh {self._get_node(data=True, num=1)[0]} {self._commands['size']}"
-            logging.debug(f"Execute command: {cmd}")
+            logger.debug(f"Execute command: {cmd}")
             try:
                 result = subprocess.run(
                     cmd,
@@ -195,48 +204,58 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
                     check=True,
                 )
                 size = result.stdout.strip().split()[0]
-                logging.info(f"Size {size} bytes")
+                logger.info(f"Size {size} bytes")
                 return size
             except Exception as e:
-                logging.error(f"Check size failed due to: {e}")
+                logger.error(f"Check size failed due to: {e}")
                 if isinstance(e, subprocess.CalledProcessError):
-                    logging.error(f"\n[stdout]\n{e.stdout}[stderr]\n{e.stderr}")
+                    logger.error(f"\n[stdout]\n{e.stdout}[stderr]\n{e.stderr}")
                 raise
 
     def _filter(self):
         srcs = self._get_node(data=True)
-        logging.info(f"Filter available_nodata nodes from {len(srcs)} available nodes")
+        if len(srcs) == 0:
+            logger.info("Skip filter due to no available node")
+            return
+        logger.info(f"Filter available_nodata nodes from {len(srcs)} available nodes")
         results = []
-        with ThreadPoolExecutor(max_workers=min(len(srcs), 128)) as executor:
+        with ThreadPoolExecutor(max_workers=self._max_workers) as executor:
             for src in srcs:
                 cmd = f"ssh {src} {self._commands['size']}"
-                logging.debug(f"Execute command: {cmd}")
+                logger.debug(f"Execute command: {cmd}")
                 results.append(executor.submit(subprocess.run, cmd, shell=True, capture_output=True, text=True))
         sizes = {}
         for res in results:
             r = res.result()
-            node = r.args.split(" ")[-1]
+            node = r.args.split(" ")[1]
             if r.returncode != 0:
                 sizes[node] = -1
             else:
                 sizes[node] = r.stdout.strip().split()[0]
-        ref_size, freq = Counter(s for s in sizes.values() if s > 4096).most_common(1)[0]
-        filtered = [node for node, s in sizes.items() if s != ref_size]
-        logging.info(f"Filtered {len(filtered)} nodes from {len(srcs)} nodes, where {freq} nodes have {ref_size} bytes.")
-        for node in filtered:
-            self._update_node_status(node, data=False, reason="Node in available state but missing data")
-        logging.info("Filtered finished")
+        counter = Counter(s for s in sizes.values() if int(s) > 0)
+        if len(counter) == 0:
+            logger.info("All available nodes have 0 files")
+        else:
+            ref_size, freq = max(counter.items(), key=lambda item: (item[1], item[0]))
+            filtered = [node for node, s in sizes.items() if s != ref_size]
+            logger.info(f"Filtered {len(filtered)} nodes from {len(srcs)} nodes, where {freq} nodes have {ref_size} bytes.")
+            for node in filtered:
+                self._update_node_status(node, data=False, reason="Node in available state but missing data")
+        logger.info("Filtered finished")
 
     def _sync(self):
         dsts = self._get_node(data=False)
         srcs = self._get_node(data=True)
-        logging.info(f"Get {len(dsts)} available_nodata nodes and {len(srcs)} available nodes")
+        if len(dsts) == 0 or len(srcs) == 0:
+            logger.info(f"Skip sync due to no {'available_nodata' if len(dsts) == 0 else 'available'} node")
+            return
+        logger.info(f"Get {len(dsts)} available_nodata nodes and {len(srcs)} available nodes")
         results = []
-        with ThreadPoolExecutor(max_workers=32) as executor:
+        with ThreadPoolExecutor(max_workers=self._max_workers) as executor:
             # leave the extra dsts to next time if len(dsts) > len(srcs)
             for src, dst in zip(srcs, dsts):
                 cmd = f"ssh {src} {self._commands['sync'].format(src=src, dst=dst)}"
-                logging.debug(f"Execute command: {cmd}")
+                logger.debug(f"Execute command: {cmd}")
                 results.append(executor.submit(subprocess.run, cmd, shell=True))
         failed = []
         for res in results:
@@ -247,8 +266,8 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
             else:
                 self._update_node_status(node, data=True, reason=f"Data synced from {r.args.split(' ')[-2]}")
         if failed:
-            logging.warning(f"Sync failed on: {' '.join(failed)}")
-        logging.info("Sync finished")
+            logger.warning(f"Sync failed on: {' '.join(failed)}")
+        logger.info("Sync finished")
 
     def _sync_loop(self):
         while not self._shutdown_evt.is_set():
@@ -259,12 +278,12 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
             if not self.lock.acquire(blocking=False):
                 continue
             self._set_state("sync")
-            logging.info("Running sync ...")
+            logger.info("Running sync loop ...")
             try:
                 self._filter()
                 self._sync()
             except Exception as e:
-                logging.error(f"Sync failed due to {e}")
+                logger.error(f"Sync loop failed due to {e}")
             finally:
                 self._set_state("idle")
                 self.lock.release()
@@ -298,7 +317,7 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
                 try:
                     data = self._parse()
                 except ValueError as e:
-                    logging.exception("Body parse failed")
+                    logger.exception("Body parse failed")
                     self.send_error(400, str(e))
                 if not data.get("path"):
                     self.send_error(400, "missing path in body")
@@ -315,7 +334,7 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
                     )
                     self._json(200, {"success": out})
                 except Exception as e:
-                    logging.exception("Download failed")
+                    logger.exception("Download failed")
                     service._delete(data.get("path"))
                     self._json(500, {"error": str(e)})
 
@@ -328,7 +347,7 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
                 try:
                     data = self._parse()
                 except ValueError as e:
-                    logging.exception("Body parse failed")
+                    logger.exception("Body parse failed")
                     self.send_error(400, str(e))
                 if not data.get("path"):
                     self.send_error(400, "missing path in body")
@@ -338,7 +357,7 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
                     out = service._delete(data.get("path"))
                     self._json(200, {"success": out})
                 except Exception as e:
-                    logging.exception("Delete failed")
+                    logger.exception("Delete failed")
                     self._json(500, {"error": str(e)})
 
             # GET /healthz, /status, /size
@@ -361,7 +380,7 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
                             out = service._size()
                             self._json(200, {"size": out})
                         except Exception as e:
-                            logging.exception("Check size failed")
+                            logger.exception("Check size failed")
                             self._json(500, {"error": str(e)})
                     else:
                         self.send_error(423, "locked")
@@ -371,17 +390,17 @@ class ClusterLocalStorageService(http.server.ThreadingHTTPServer):
 
             # (optional) quieter log
             def log_message(self, fmt, *args):
-                logging.info("%s - %s", self.client_address[0], fmt % args)
+                logger.info("%s - %s", self.client_address[0], fmt % args)
 
         return H
 
     def shutdown(self):
         """Override shutdown to stop the sync thread first."""
-        logging.info("Shutdown initiated ...")
+        logger.info("Shutdown initiated ...")
         self._shutdown_evt.set()
         # stops _sync_loop()
         self._sync_thread.join()
-        logging.info("Shutdown complete")
+        logger.info("Shutdown complete")
 
     @classmethod
     def run(cls, host="0.0.0.0", port=7000):
