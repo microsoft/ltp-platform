35d31183cef346466b893409d06d79c6ab9b6a55
Rui Gao <ruigao@microsoft.com>
2025-09-08 11:29:50 +0800

    add blobfuse checking and updating for VMSS

--- contrib/aks/scripts/install-fuse.sh
+++ contrib/aks/scripts/install-fuse.sh
@@ -7,3 +7,12 @@ set -xe
 
 DEBIAN_FRONTEND=noninteractive apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get install libfuse3-dev fuse3 blobfuse2 -y || echo "Failed to install fuse"
+INSTALLED_VERSION=$(blobfuse2 --version | grep -oP '\d+\.\d+\.\d+')
+REQUIRED_VERSION="1.5.0"
+
+if dpkg --compare-versions "$INSTALLED_VERSION" "lt" "$REQUIRED_VERSION"; then
+    echo "Updating blobfuse2 to a version newer than $REQUIRED_VERSION"
+    DEBIAN_FRONTEND=noninteractive apt-get install --only-upgrade blobfuse2 -y || echo "Failed to update blobfuse2"
+else
+    echo "blobfuse2 is already up-to-date (version $INSTALLED_VERSION)"
+fi
