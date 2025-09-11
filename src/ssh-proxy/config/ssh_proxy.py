9b7d7972d094928c4cb4fe5c5a74c0a40a36c409
Rui Gao <ruigao@microsoft.com>
2025-09-11 11:03:50 +0800

    fix PR comments

--- src/ssh-proxy/config/ssh_proxy.py
+++ src/ssh-proxy/config/ssh_proxy.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+
 import copy
 
 class SshProxy(object):
--- src/utilization-reporter/src/cluster_utilization_reporter.py
+++ src/utilization-reporter/src/cluster_utilization_reporter.py
@@ -1,3 +1,7 @@
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+
+
 import logging
 import os
 import requests
