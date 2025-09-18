d403e48deb269505f188adab4871f939596e4ff2
Rui Gao <ruigao@microsoft.com>
2025-09-19 07:24:47 +0800

    update the required version to the correct one

--- contrib/aks/scripts/install-fuse.sh
+++ contrib/aks/scripts/install-fuse.sh
@@ -8,7 +8,7 @@ set -xe
 DEBIAN_FRONTEND=noninteractive apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get install libfuse3-dev fuse3 blobfuse2 -y || echo "Failed to install fuse"
 INSTALLED_VERSION=$(blobfuse2 --version | grep -oP '\d+\.\d+\.\d+')
-REQUIRED_VERSION="1.5.0"
+REQUIRED_VERSION="2.5.0"
 
 if dpkg --compare-versions "$INSTALLED_VERSION" "lt" "$REQUIRED_VERSION"; then
     echo "Updating blobfuse2 to a version newer than $REQUIRED_VERSION"
