2b1bb6a96a14838a00e0dd716c3d8d623ef3a1a7
Lei <lequ@microsoft.com>
2025-10-14 16:48:29 -0700

    code refactor: stage 0

--- src/copilot-chat/src/copilot_agent/__main__.py
+++ src/copilot-chat/src/copilot_agent/__main__.py
@@ -4,13 +4,11 @@
 """Main module."""
 
 from .copilot_service import CoPilotService
-from .copilot_conversation import CoPilotConversation
 
 
 def main():
     """Main function."""
-    copilot_conversation = CoPilotConversation()
-    api = CoPilotService(copilot_conversation)
+    api = CoPilotService()
     api.run()
 
 
