4506329567849e92a7d557c2c8ec0a4061016aa5
Lei <lequ@microsoft.com>
2025-10-16 17:09:54 -0700

    code refactor: stage 4, change ltp into a class for easier state management

--- src/copilot-chat/src/copilot_agent/utils/__init__.py
+++ src/copilot-chat/src/copilot_agent/utils/__init__.py
@@ -33,6 +33,7 @@ from .promql import (
 )
 from .push_frontend import(
     push_frontend_event,
+    set_thread_llm_session,
 )
 from .query import (
     gen_kusto_query_fallback_pseudo,
@@ -105,4 +106,5 @@ __all__ = [
     'push_frontend_event',
     'retry_function',
     'save_to_csv',
+    'set_thread_llm_session',
 ]
