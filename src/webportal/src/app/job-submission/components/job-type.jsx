92f5646ad554a845111956ed94738bc62a1037d4
Zhongxin Guo <zhogu@microsoft.com>
2025-11-06 19:32:09 -0800

    update

--- src/webportal/src/app/job-submission/components/job-type.jsx
+++ src/webportal/src/app/job-submission/components/job-type.jsx
@@ -25,17 +25,6 @@ export const JobType = React.memo(props => {
     (_, item) => {
       if (onChange !== undefined) {
         onChange(item.text);
-        if (item.text === 'inference') {
-          alert(
-            `Inference jobs have three forced parameters: 
-  1. INTERNAL_SERVER_IP=$PAI_HOST_IP_taskrole_0 : Fixed value, used to inference service IP
-  2. INTERNAL_SERVER_PORT=$PAI_PORT_LIST_taskrole_0_http : Fixed value used to inference service port
-  3. API_KEY="...": A random generated string, can be set arbitrarily)
-
-The three parameters will be automatically added to your job parameters upon switching to inference job type.
-  `,
-          );
-        }
       }
     },
     [onChange],
