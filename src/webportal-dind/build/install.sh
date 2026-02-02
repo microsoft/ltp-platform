d5af0ecd7112c5958da281177d49411cc3dde8b0
Rui Gao <ruigao@microsoft.com>
2026-02-02 07:57:47 +0000

    update buildx to 0.31.1 for webportal-dind

--- src/webportal-dind/build/install.sh
+++ src/webportal-dind/build/install.sh
@@ -9,7 +9,7 @@ set -eux
 DOCKER_CHANNEL="stable"
 DOCKER_VERSION="29.2.0"
 DOCKER_COMPOSE_VERSION="v5.0.2"
-BUILDX_VERSION="v0.31.0"
+BUILDX_VERSION="v0.31.1"
 
 # Logging functions
 function log_info() {
